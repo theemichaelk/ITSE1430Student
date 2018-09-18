7d6e08f5a4acaf55226380a450666241e04e2649
michael kaswatuka <theemichael@outlook.com>
2018-09-17 19:06:30 -0500

    today's class 9/17/2018

---
+++ Classwork/Section2/ITSE1430.MovieLot/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSE1430.MovieLot
+{
+    class Movie
+    {
+        public string Name;
+        //public System.String Name;
+    }
+}
