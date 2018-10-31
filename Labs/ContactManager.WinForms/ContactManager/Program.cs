38d7d8501e6c9b5b6a844c254eb5845eb45f02f4
michael kaswatuka <theemichael@outlook.com>
2018-10-31 17:45:31 -0500

    contact manager-2

--- Labs/ContactManager.WinForms/ContactManager/Program.cs
+++ Labs/ContactManager.WinForms/ContactManager/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Mike Kaswatuka
+ * ITSE 1430-20630 (Introduction to C# Programming) 
+ * 10/31/2018 
+ * Professor Michael Taylor*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
